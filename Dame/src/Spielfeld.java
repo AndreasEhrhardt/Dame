941d585843a10158c74c220fe785b0bb46363165
AndreasEhrhardt <andreas.ehrhardt@student.reutlingen-university.de>
2015-10-06 21:56:26 +0200

    Added properties and setter/getter

--- Dame/src/Spielfeld.java
+++ Dame/src/Spielfeld.java
@@ -1,6 +1,73 @@
 //###########################################################
+//## Imports
+
+
+//###########################################################
 //## Class
 
 public class Spielfeld {
+
+	//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+	//++ Properties
+
+	private String ID;
+	private Spielfigur figur;
+
+	//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+	//++ Constructor
+
+	public Spielfeld(){
+		
+	}
+	
+	public Spielfeld(String ID){
+		this.setID(ID);
+	}
+	
+	public Spielfeld(String ID, Spielfigur figur){
+		this.setFigur(figur);
+		this.setID(ID);
+	}
+	
+	//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+	//++ Methods
+
+
+	//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+	//++ Methods ( Getter)
+
+	/**
+	 * @return
+	 */
+	public Spielfigur getFigur(){
+		return this.figur;
+	}
+	
+	/**
+	 * @return
+	 */
+	public String getID(){
+		return this.ID;
+	}
+
+	//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+	//++ Methods ( Setter)
+	
+	/**
+	 * @param figur
+	 */
+	public void setFigur(Spielfigur figur){
+		this.figur = figur;
+	}
+
+	/**
+	 * @param ID
+	 */
+	public void setID(String ID){
+		this.ID = ID;
+	}
+
+	//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+	//++ Methods (Override)
 
 }
