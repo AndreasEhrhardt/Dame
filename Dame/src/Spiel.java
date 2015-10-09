d7479f87254d6322ac16107a7b0c675af5eef772
AndreasEhrhardt <andreas.ehrhardt@student.reutlingen-university.de>
2015-10-09 13:48:42 +0200

    Add method to gameboard-creation

--- Dame/src/Spiel.java
+++ Dame/src/Spiel.java
@@ -21,40 +21,51 @@ public class Spiel{
 	public Spiel(){
 		gamer = new Spieler[2];
 
-		try{
-			// Create keyboard reader
-			Scanner sc = new Scanner(System.in);
+		try{			
+			// Create gameboard
+			this.createGameBoard();
 			
-			//+++++++++++++++++++++++++++++++++++++++++++
-			//++ Get field size and create new gameboard
-			
-			// Define min field size and max field size
-			int maxField = 20, minField = 4;
-			System.out.println("Bitte Spielfeldgröße angeben (" + minField + "-" + maxField + "):");
-			int fieldCount = 8;
-			for(int i = 0; i <= 1000; i++){
-				// Read next field size
-				fieldCount = sc.nextInt();
-				// If size is valid, leave loop
-				if(fieldCount >= minField && fieldCount <= maxField) break;
-				// Check if endless loop
-				if(i == i){ 
-					System.out.println("No valid number detected, we will choose the value 8");
-					fieldCount = 8;
-				}
-			}
-			this.gameboard = new Spielbrett(fieldCount);
-			
-			// Get player 1 informations
-			
+			// Create gamer 1
 		}
 		catch(Exception e){
 			System.out.println("Some errors appear");
+			
 		}
 	}
 
 	//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 	//++ Methods
+	
+	/**
+	 * 
+	 */
+	private void createGameBoard() throws Exception
+	{
+		// Create scanner
+		Scanner sc = new Scanner(System.in);
+		
+		// Get field size
+		int maxField = 20, minField = 4;
+		System.out.println("Bitte Spielfeldgröße angeben (" + minField + "-" + maxField + "):");
+		int fieldCount = 8;
+		for(int i = 0; i <= 1000; i++){
+			// Read next field size
+			fieldCount = sc.nextInt();
+			// If size is valid, leave loop
+			if(fieldCount >= minField && fieldCount <= maxField) break;
+			// Check if endless loop
+			if(i == 1000){ 
+				System.out.println("No valid number detected, we will choose the value 8");
+				fieldCount = 8;
+			}
+		}
+		
+		// Create gameboard
+		this.gameboard = new Spielbrett(fieldCount);
+		
+		// Close scanner
+		sc.close();
+	}
 
 	
 	//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
