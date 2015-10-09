f9f68bf13d3ad11e59cafec2d2b34898951e734a
Schie <Schie.Xiong@Student.Reutlingen-University.de>
2015-10-09 14:36:17 +0200

    class Spiel gamer method

--- Dame/src/Spiel.java
+++ Dame/src/Spiel.java
@@ -6,6 +6,7 @@ import java.util.*;
 //###########################################################
 //## Class
 
+
 public class Spiel{
 
 	//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
@@ -14,7 +15,7 @@ public class Spiel{
 	private Spielbrett gameboard;
 	private Spieler gamer [];
 	private Spieler currentGamer;
-
+	static int maxLoopCount = 10;
 	//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 	//++ Constructor
 
@@ -46,9 +47,9 @@ public class Spiel{
 		
 		// Get field size
 		int maxField = 20, minField = 4;
-		System.out.println("Bitte Spielfeldgröße angeben (" + minField + "-" + maxField + "):");
+		System.out.print("Bitte Spielfeldgröße angeben (" + minField + "-" + maxField + "):");
 		int fieldCount = 8;
-		for(int i = 0; i <= 1000; i++){
+		for(int i = 0; i <= maxLoopCount; i++){
 			// Read next field size
 			fieldCount = sc.nextInt();
 			// If size is valid, leave loop
@@ -67,7 +68,25 @@ public class Spiel{
 		sc.close();
 	}
 
-	
+	private Spieler createGamer(){
+		//create Scanner
+		Scanner sc = new Scanner(System.in);
+		
+		//create Gamer
+		int gamerID;
+		for(int i = 0; i <= maxLoopCount; i++){
+			System.out.println("Spieler oder KI?");
+			System.out.println("Spieler = 1, KI = 2");
+			
+			gamerID = sc.nextInt();
+			
+		}
+		if(gamerID == 1){
+			return  
+		}else if(gamerID == 2){
+			
+		}
+	}
 	//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 	//++ Methods ( Getter)
 
