af4fd9b2c60c152f990cece1aab73e37a58d0e92
AndreasEhrhardt <andreas.ehrhardt@student.reutlingen-university.de>
2015-10-13 10:34:53 +0200

    Upstream iBediener and jSpielfigur

---
+++ Dame/src/jSpielfigur.java
@@ -0,0 +1,13 @@
+import static org.junit.Assert.*;
+import java.awt.*;
+
+import org.junit.Test;
+
+public class jSpielfigur {
+	
+	@Test
+	public void constructors(){
+		FarbEnum farbe = FarbEnum.weiﬂ;
+		Spielfigur figur = new Spielfigur(farbe, new Point());
+	}
+}
