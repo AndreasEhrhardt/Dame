842f2779a530c96e9c838fb7b4820da7cafd11cb
Schie <Schie.Xiong@Student.Reutlingen-University.de>
2015-10-20 09:49:56 +0200

    class KI_Dame created

---
+++ Dame/src/KI_Dame.java
@@ -0,0 +1,20 @@
+
+public class KI_Dame extends KI{
+	//Properties
+	private KI ki_Dame;
+	//Constructor
+	public KI_Dame(){
+		setKi_Dame();
+	}
+	
+	
+	//Getter and Setter
+	public void setKi_Dame(){
+		this.ki_Dame = new KI();
+	}
+	
+	//methods
+	
+	
+	
+}
