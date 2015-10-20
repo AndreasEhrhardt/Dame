91619cf60f2d02d6b60a1787874d6c57ded45ddc
Schie <Schie.Xiong@Student.Reutlingen-University.de>
2015-10-20 15:58:59 +0200

    Ki_Dame changed

--- Dame/src/KI_Dame.java
+++ Dame/src/KI_Dame.java
@@ -1,17 +1,15 @@
 
 public class KI_Dame extends KI{
 	//Properties
-	private KI ki;
+	
 	//Constructor
 	public KI_Dame(){
-		setKi_Dame();
+		
 	}
 	
 	
 	//Getter and Setter	
-	public void setKi_Dame(){
-		this.ki = new KI();
-	}
+	
 	
 	//methods
 	
