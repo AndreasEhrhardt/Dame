56932817c9cd2b4df880f78c128b8a7289bdf22b
Schie <Schie.Xiong@Student.Reutlingen-University.de>
2015-10-27 14:06:50 +0100

    KI_Dame methods created

--- Dame/src/KI_Dame.java
+++ Dame/src/KI_Dame.java
@@ -1,18 +1,33 @@
+import java.awt.*;
 
 public class KI_Dame extends KI{
 	//Properties
-	
+	private Spieler kiPlayer;
 	//Constructor
 	public KI_Dame(){
+		setKiPlayer();
+	}
+	//getter/Setter
+	
+	
+	private void setKiPlayer(){
+		this.kiPlayer = new Spieler();
+	}
+	//methods
+	//pusten
+	//public 
+	//check if move is valid
+	public boolean isFree(){
 		
+		return false;
 	}
 	
-	
-	//Getter and Setter	
-	
-	
-	//methods
-	
-	
-	
+	//move to empty space
+	public void move(Spiel game){
+		//exception abfangen
+		//if position != free -> nextPosition
+		//if position == free -> setze stein 
+		//spiel.move
+		
+	}
 }
