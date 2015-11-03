d8479eb9401d65524fbe61ec8677cb7bfbeedd4d
Schie <Schie.Xiong@Student.Reutlingen-University.de>
2015-11-03 13:09:57 +0100

    interfaces changed

--- Dame/src/Interfaces/iBediener.java
+++ Dame/src/Interfaces/iBediener.java
@@ -2,15 +2,12 @@ package Interfaces;
 import GameLogic.Spieler;
 
 public interface iBediener {
-	void nextMove();
 	
 	void outputGameboardCSV();
 	
 	int getGameboardSize();
 	
 	void loadingScreen();
-	void loadGame();
-	void saveGame();
 	
 	boolean gameFinished();
 	boolean askNewGame();
---
+++ Dame/src/Interfaces/iDatenzugriff.java
@@ -0,0 +1,6 @@
+package Interfaces;
+
+public interface iDatenzugriff {
+	void saveGame();
+	void loadGame();
+}
