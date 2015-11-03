9100175b667ac6a3437b80530b074ed9a4e6a8a0
Schie <Schie.Xiong@Student.Reutlingen-University.de>
2015-11-03 14:36:03 +0100

    method in interface iDatenzugriff

--- Dame/src/Interfaces/iDatenzugriff.java
+++ Dame/src/Interfaces/iDatenzugriff.java
@@ -1,9 +1,19 @@
 package Interfaces;
 
+import java.io.File;
+
 import GameLogic.Spiel;
 
 public interface iDatenzugriff {
+	public String saveGameName = "";
+	
 	void saveGame(Spiel game);
 	void loadGame(Spiel game);
-	boolean haveSaveGame();
+	public static boolean haveSaveGame(){
+		File f = new File(saveGameName);
+		if(!f.exists() || f.isDirectory()){
+			return false;
+		}
+		return true;
+	}
 }
