98dd1c5be2a233402b47defda56f21e96144b4f3
AdinaLeuchtenberg <adina_sofie.leuchtenberg@student.reutlingen-university.de>
2015-11-09 18:34:15 +0100

    SpielTestSpeichern, SpielTestLaden for serialised savin/loading

--- Dame/src/SavegameManager/SpielTestLaden.java
+++ Dame/src/SavegameManager/SpielTestLaden.java
@@ -1,13 +1,16 @@
 package SavegameManager;
 
 import GameLogic.*;
+import Interfaces.iDatenzugriff;
 
 public class SpielTestLaden {
 
 	public static void main(String[] args) {
 
 		Spiel game = new Spiel();
+		iDatenzugriff serialisiert = new DatenzugriffSerialisiert();
 		game.load("./", "dame");
+		
 		game.outputGameboardCSV();
 		game.getCurrentGamer().move(game, null);
 		game.outputGameboardCSV();
@@ -16,5 +19,17 @@ public class SpielTestLaden {
 		game.getCurrentGamer().move(game, null);
 		game.outputGameboardCSV();
 		game.getCurrentGamer().move(game, null);
+		game.outputGameboardCSV();
+		System.out.println("serialisiert");
+		serialisiert.loadGame("./", "dameSerialisiert", game);
+		game.outputGameboardCSV();
+		game.getCurrentGamer().move(game, null);
+		game.outputGameboardCSV();
+		game.getCurrentGamer().move(game, null);
+		game.outputGameboardCSV();
+		game.getCurrentGamer().move(game, null);
+		game.outputGameboardCSV();
+		game.getCurrentGamer().move(game, null);
+		game.outputGameboardCSV();
 	}
 }
--- Dame/src/SavegameManager/SpielTestSpeichern.java
+++ Dame/src/SavegameManager/SpielTestSpeichern.java
@@ -12,6 +12,7 @@ public class SpielTestSpeichern {
 	public static void main(String[] args) {
 		try{
 			Spiel newGame = new Spiel();
+			iDatenzugriff serialised = new DatenzugriffSerialisiert();
 			newGame.setPlayer(1, new Spieler(new KI_Dame(),FarbEnum.weiß));
 			newGame.setPlayer(2, new Spieler(new KI_Dame(),FarbEnum.schwarz));
 			newGame.setCurrentGamer(FarbEnum.weiß);
@@ -22,6 +23,8 @@ public class SpielTestSpeichern {
 			newGame.getCurrentGamer().move(newGame, null);
 			newGame.outputGameboardCSV();
 			newGame.save("./", "dame");
+			serialised.saveGame("./", "dameSerialisiert", newGame);
+			
 		}
 		catch(Exception e){
 			System.out.println("Shit happens: " + e.getMessage());
