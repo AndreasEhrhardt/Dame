a30564cf241045536b2db884f5600cb5b796c222
Andreas Ehrhardt <andreas.ehrhardt@student.reutlingen-university.de>
2015-11-12 11:41:31 +0100

    Add game to MainFrame

--- Dame/src/GUI/MainFrame.java
+++ Dame/src/GUI/MainFrame.java
@@ -31,6 +31,8 @@ public class MainFrame extends JFrame {
 	private MainFrame(){
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	
+		this.game = new Spiel();
+		
 		mp = new MainPanel(this);
 		this.setContentPane(mp);
 		
@@ -58,6 +60,10 @@ public class MainFrame extends JFrame {
 
 	public MainPanel getMainPanel(){
 		return this.mp;
+	}
+	
+	public Spiel getGame(){
+		return this.game;
 	}
 
 	//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
