bdc1bff85157729a0bca8b2bc62738e6eb81496c
Andreas Ehrhardt <andreas.ehrhardt@student.reutlingen-university.de>
2015-11-14 14:21:02 +0100

    Changed board button size

--- Dame/src/GUI/Board.java
+++ Dame/src/GUI/Board.java
@@ -4,54 +4,18 @@ import java.awt.*;
 import javax.swing.*;
 
 import GameLogic.Spiel;
-public class Board extends MainPanelComponent {
 
-	public Board(MainPanel mp) {
-		super(mp);
+public class Board extends JPanel {
+
+	public Board() {				
+		Spiel game = MainFrame.globalPointer.getGame();
+		int gameboardSize = game.getGameboardSize();
+		JButton[][] buttons = new JButton[gameboardSize][gameboardSize];
+		int buttonSize = (int)((double)60 - ((gameboardSize - 8) * 2.5));
+		Dimension dimension = new Dimension(buttonSize,buttonSize);
+		this.setLayout(new GridLayout(gameboardSize + 1, gameboardSize + 1));
 		
-		JPanel jPanel = new JPanel();
-		
-		Spiel game = mp.getMainFrame().getGame();
-		int size = game.getGameboardSize();
-		JButton[][] buttons = new JButton[size][size];
-		Dimension dimension;
-		
-		//size of buttons
-		switch(size) {
-		case 8: 
-			jPanel.setLayout(new GridLayout(0, 9));
-			dimension = new Dimension(60,60);
-			break;
-		case 10: 
-			jPanel.setLayout(new GridLayout(0,11));
-			dimension = new Dimension(55,55);
-			break;
-		case 12: 
-			jPanel.setLayout(new GridLayout(0, 13));
-			dimension = new Dimension(50,50);
-			break;
-		case 14: 
-			jPanel.setLayout(new GridLayout(0, 15));
-			dimension = new Dimension(45,45);
-			break;
-		case 16: 
-			jPanel.setLayout(new GridLayout(0, 17));
-			dimension = new Dimension(40,40);
-			break;
-		case 18: 
-			jPanel.setLayout(new GridLayout(0, 19));
-			dimension = new Dimension(35,35);
-			break;
-		case 20: 
-			jPanel.setLayout(new GridLayout(0, 21));
-			dimension = new Dimension(30,30);
-			break;	
-		default: 
-			jPanel.setLayout(new GridLayout(0, 13));
-			dimension = new Dimension(12,12);
-		}
-		
-		//colour of buttons
+		// Colour of buttons
 		for (int i = 0; i < buttons.length; i++) {
 			for(int j = 0; j < buttons[i].length; j++) {
 				JButton button = new JButton();
@@ -67,24 +31,20 @@ public class Board extends MainPanelComponent {
 		}
 		
 		
-		jPanel.add(new JLabel(""));
-		//letters of board
+		this.add(new JLabel(""));
+		// Letters of board
 		for (int k = 0; k < buttons.length; k++) {
-			jPanel.add(new JLabel(String.valueOf((char) (65 + (buttons.length - 1) - k))), SwingConstants.CENTER);
+			this.add(new JLabel(String.valueOf((char) (65 + (buttons.length - 1) - k))), SwingConstants.CENTER);
 		}
 		
-		//numbers of board
+		// Numbers of board
 		for (int k = 0; k < buttons.length; k++) {
 			for (int j = 0; j < buttons.length; j++) {
 				if (j == 0) {
-					jPanel.add(new JLabel("" + (size - k), SwingConstants.CENTER));
+					this.add(new JLabel("" + (gameboardSize - k), SwingConstants.CENTER));
 				}
-				jPanel.add(buttons[k][j]);
+				this.add(buttons[k][j]);
 			}
 		}
 	}
-	
-	
-	
-
 }
