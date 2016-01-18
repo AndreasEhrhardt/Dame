3223ab37e77bbbfc6464e6c5e623dce98c35ec52
AdinaLeuchtenberg <adina_sofie.leuchtenberg@student.reutlingen-university.de>
2016-01-18 20:26:17 +0100

    serialized is working!

--- Dame/src/Servlet/LoadServlet.java
+++ Dame/src/Servlet/LoadServlet.java
@@ -20,104 +20,100 @@ import SavegameManager.DatenzugriffXML;
 @WebServlet("/LoadServlet")
 public class LoadServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public LoadServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	public LoadServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// Get Sessions
 		HttpSession session = request.getSession(true);
 		request.getRequestDispatcher("/WEB-INF/STYLES/Style.jsp").include(request, response);
 		request.getRequestDispatcher("/WEB-INF/Header.jsp").include(request, response);
 		request.getRequestDispatcher("/WEB-INF/load.jsp").include(request, response);
-		
-		SpielBean game = (SpielBean)this.getServletConfig().getServletContext().getAttribute("GAME");
+
+		SpielBean game = (SpielBean) this.getServletConfig().getServletContext().getAttribute("GAME");
 		this.getServletConfig().getServletContext().removeAttribute("GAME");
 		game = null;
-		
+
 		String filetype = "";
 		String path = "";
 		String filename = "";
-		
-		if(request.getParameter("Laden") != null) {
-		filetype = request.getParameter("Laden");
+
+		if (request.getParameter("Laden") != null) {
+			filetype = request.getParameter("Laden");
 		}
-		
-		if(request.getParameter("Path") != null) {
+
+		if (request.getParameter("Path") != null) {
 			path = request.getParameter("Path");
 		}
-		
-		if(request.getParameter("Dateiname") != null) {
-		filename = request.getParameter("Dateiname");
+
+		if (request.getParameter("Dateiname") != null) {
+			filename = request.getParameter("Dateiname");
 		}
-		
-	System.out.println(filetype);
-	System.out.println(path);
-	System.out.println(filename);
-			if(filetype.matches("CSV")){
-				System.out.println("inIfabfrage");
-			try  {
-			
+
+		System.out.println(filetype);
+		System.out.println(path);
+		System.out.println(filename);
+		if (filetype.matches("CSV")) {
 			iDatenzugriff csv = new DatenzugriffCSV();
 			game = new SpielBean();
 			csv.loadGame(path, filename, game);
-			
-			} catch (Exception e)
-			{
-				System.out.println(e);
-				e.printStackTrace();
-				System.out.println("CsvTest");
-			}
-			// Output information
-			String player1 = game.getPlayer(1).getName();
-			String player2 = game.getPlayer(2).getName();
-			
-			// Save game in application scope
-			this.getServletConfig().getServletContext().setAttribute("GAME", game);
+		}
 
-			// Set player as active gamer
-			this.getServletConfig().getServletContext().setAttribute("P1_SESSION", false);
-			this.getServletConfig().getServletContext().setAttribute("P2_SESSION", false);
-			if(game.getPlayer(1).getKi() == null){
-				this.getServletConfig().getServletContext().setAttribute("P1_SESSION", true);
-				session.setAttribute("NAME", player1);
-			}
-			else if(game.getPlayer(2).getKi() == null){
-				this.getServletConfig().getServletContext().setAttribute("P2_SESSION", true);
-				session.setAttribute("NAME", player2);
-			}
-		
-			// Set gameid
-			if(session.getAttribute("NAME") != null) session.setAttribute("GAME_ID", game.getID());
+		if (filetype.matches("Serialisiert")) {
+			iDatenzugriff ser = new DatenzugriffSerialisiert();
+			game = new SpielBean();
+			ser.loadGame(path, filename, game);
+		}
 
-			// Output information
-			System.out.println("Gameobject created!");
-			
-			session.setAttribute("GAME", game);
-			response.sendRedirect(request.getContextPath() + "/BacktoGameServlet");
-		
-			}
-		
-		
-		
-		
-		
+		// Output information
+		String player1 = game.getPlayer(1).getName();
+		String player2 = game.getPlayer(2).getName();
+
+		// Save game in application scope
+		this.getServletConfig().getServletContext().setAttribute("GAME", game);
+
+		// Set player as active gamer
+		this.getServletConfig().getServletContext().setAttribute("P1_SESSION", false);
+		this.getServletConfig().getServletContext().setAttribute("P2_SESSION", false);
+		if (game.getPlayer(1).getKi() == null) {
+			this.getServletConfig().getServletContext().setAttribute("P1_SESSION", true);
+			session.setAttribute("NAME", player1);
+		} else if (game.getPlayer(2).getKi() == null) {
+			this.getServletConfig().getServletContext().setAttribute("P2_SESSION", true);
+			session.setAttribute("NAME", player2);
+		}
+
+		// Set gameid
+		if (session.getAttribute("NAME") != null)
+			session.setAttribute("GAME_ID", game.getID());
+
+		// Output information
+		System.out.println("Gameobject created!");
+
+		session.setAttribute("GAME", game);
+		response.sendRedirect(request.getContextPath() + "/BacktoGameServlet");
+
 		request.getRequestDispatcher("/WEB-INF/Footer.jsp").include(request, response);
 
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
